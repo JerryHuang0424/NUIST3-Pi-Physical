2c28d5691e8ca4f7a28d33545eabd8e6583c904c
JerryHuang0424 <17321421572@qq.com>
2025-04-02 02:35:33 +0100

    Complete the quiz2.py file with more questions

--- quiz2.py
+++ quiz2.py
@@ -1,3 +1,6 @@
+import RPi.GPIO as GPIO
+import time
+
 # NUist Quiz Game in Python
 def quiz():
 	print("Welcome to the Animal Quiz")
@@ -23,12 +26,26 @@ def quiz():
 			b. ints to strings
 			c. floats and ints to strings
 			d. ints to floats
+		""",
+		"""4. The best structure for implementing a multi-way decision in Python is:
+			a. if
+			b. if-else
+			c. if-elif-else
+			d. try
+		""",
+		"""5. What statement can be executed in the body of a loop to cause it to terminate?
+			a. if
+			b. exit
+			c. continue
+			d. break
 		"""
 	]
 
 	answers = [
 		"c",
 		"d",
+		"d",
+		"c",
 		"d"
 	]
 
@@ -41,14 +58,30 @@ def quiz():
 		if user_answer == answers[i]:
 			print("Correct!")
 			score += 1
+			print("LED ON")
+			GPIO.output(17,GPIO.HIGH)
+			time.sleep(1)
+			print("LED off")
+			GPIO.output(17,GPIO.LOW)
+			
 		else:
 			print("Incorrect!")
+			print("LED ON")
+			GPIO.output(18,GPIO.HIGH)
+			time.sleep(1)
+			print("LED off")
+			GPIO.output(18,GPIO.LOW)
 
 	# Provide final score
 	print("Quiz completed!")
 	print(f"You got {score}/{len(questions)} questions correct.")
 	
 # Run the quiz function
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(17,GPIO.OUT)
 quiz()
 
 
